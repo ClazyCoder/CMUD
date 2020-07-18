e49b015df3162aa9dee0b772f88ef1f63c762e6b
ClazyCoder <xots899@naver.com>
2020-07-18 19:12:07 +0900

    메소드 틀 추가

--- CMUD/CMUD/Managers.cs
+++ CMUD/CMUD/Managers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CMUD
@@ -11,13 +12,23 @@ namespace CMUD
         {
             mapList = new Map[10];
         }
+        public void LoadMaps(string filename)
+        {
+            // TODO : 파일 열기(형식구상)
+        }
     }
     class ObjectManager
     {
+        int objCount;
         Object[] objList;
         public ObjectManager()
         {
+            objCount = 0;
             objList = new Object[100];
+        }
+        public void addObject(Object obj)
+        {
+            this.objList[objCount++] = obj;
         }
     }
 }
