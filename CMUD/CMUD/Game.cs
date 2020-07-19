1e2d2a27059ab0c5ed705fd1ab020823b590874a
ClazyCoder <xots899@naver.com>
2020-07-19 21:39:04 +0900

    서버형식으로 클래스 수정

--- CMUD/CMUD/Game.cs
+++ CMUD/CMUD/Game.cs
@@ -2,27 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
 
 namespace CMUD
 {
     class Game
     {
+        bool _isRunning;
+        TcpListener _server;
+        List<Client> _clients;
         Game()
         {
-
+            _isRunning = false;
+            _clients = new List<Client>();
         }
         void Calc()
         {
 
         }
-        void Render()
-        {
-
-        }
         public void Start()
         {
-            Calc();
-            Render();
+            _isRunning = true;
+            Thread ServerThread = new Thread(new ThreadStart(ServerThreadRoutine));
+        }
+        public void ServerThreadRoutine()
+        {
+            _server = new TcpListener(new IPEndPoint(IPAddress.Any,5252));
+            _server.Start();
+            while (_isRunning)
+            {
+                Client client = new Client(_server.AcceptTcpClient());
+                lock (_clients)
+                {
+                    _clients.Add(client);
+                }
+            }
+        }
+    }
+    class Client
+    {
+        TcpClient client;
+        Player player;
+        public Client(TcpClient client)
+        {
+
         }
     }
 }
