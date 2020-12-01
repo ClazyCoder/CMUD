526351ec68251922e4d3987e1e7f67884ca23c8a
ClazyCoder <xots899@naver.com>
2020-12-01 10:45:07 +0900

    매니저 클래스 싱글톤 패턴 적용. 배열형태 저장방식에서 List<>로 변경.

--- CMUD/CMUD/Managers.cs
+++ CMUD/CMUD/Managers.cs
@@ -2,51 +2,56 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Data.Sqlclient;
+using System.Collections;
+using System.Data.SqlClient;
 
 namespace CMUD
 {
     class MapManager
     {
-        Map[] mapList;
+        List<Map> mapList;
         int _mapCount;
-        public MapManager()
+
+        private static readonly Lazy<MapManager> _instance = new Lazy<MapManager>(() => new MapManager());
+        private MapManager()
         {
-            mapList = new Map[10];
+            mapList = new List<Map>();
+        }
+        public static MapManager Instance
+        {
+            get { return _instance.Value; }
         }
         public void LoadMap(string filename)
         {
-            int[,] map = new int[100, 100];
-            string[] lines = File.ReadAllLines(@filename);
-            string mapName = lines[0];
-            for(int i=1;i<lines.Length; i++)
-            {
-                for(int j=0;j<lines.Length; j++)
-                {
-                    map[i-1, j] = lines[i][j];
-                }
-            }
-            mapList[_mapCount++] = new Map(mapName,map);
+            // TODO : 파일로부터 Map 불러오기
         }
         public void LoadMapsFromDB()
         {
-            // TODO : DataBase에서 map 정보 불러오기
+            // TODO : DataBase에서 Map 정보 불러오기
         }
     }
     class ObjectManager
     {
-        int objCount;
-        Object[] objList;
+        int ObjectCount
+        {
+            get { return objList.Count; }
+        }
+        List<Object> objList;
         Random _dice;
-        public ObjectManager()
+
+        private static readonly Lazy<ObjectManager> _instance = new Lazy<ObjectManager>(() => new ObjectManager());
+        private ObjectManager()
         {
             _dice = new Random();
-            objCount = 0;
-            objList = new Object[100];
+            objList = new List<Object>();
+        }
+        public static ObjectManager Instance
+        {
+            get { return _instance.Value; }
         }
         public void AddObject(Object obj)
         {
-            this.objList[objCount++] = obj;
+            this.objList.Add(obj);
         }
         public void DoRandomEvents()
         {
@@ -71,20 +76,34 @@ namespace CMUD
         }
         public Object FindObject(string name)
         {
+            // TODO : 오브젝트 찾기(이름으로)
             Object temp = new Object();
             return temp;
         }
         public Object FindObject(int x, int y)
         {
+            // TODO : 오브젝트 찾기(특정위치)
             Object temp = new Object();
             return temp;
         }
     }
     public class DatabaseManager
     {
-        string ip; // 디비 아이피
-        string id; // 디비 아이디
-        string pw; // 디비 비밀번호
-        
+        string ip;          // 디비 아이피
+        string port;        // 디비 포트
+        string id;          // 디비 아이디
+        string pw;          // 디비 비밀번호
+        SqlConnection conn; // 디비 커넥션
+
+        private static readonly Lazy<DatabaseManager> _instance = new Lazy<DatabaseManager>(() => new DatabaseManager());
+        private DatabaseManager()
+        {
+            // TODO : DB 연결 및 초기화
+            conn = new SqlConnection();
+        }
+        public static DatabaseManager Instance
+        {
+            get { return _instance.Value; }
+        }
     }
 }
