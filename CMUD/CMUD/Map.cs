f7e673d8f8ecd477deea014b9a8bf9b0e7bfb9b3
ClazyCoder <xots899@naver.com>
2021-01-20 17:19:34 +0900

    맵 형태 변경.

--- CMUD/CMUD/Map.cs
+++ CMUD/CMUD/Map.cs
@@ -8,16 +8,16 @@ namespace CMUD
     class Map
     {
         string _mapName;
-        int[,] _map;
+        int[] _map;
         public Map(string _mapName)
         {
             this._mapName = _mapName;
-            this._map = new int[10, 10];
+            this._map = new int[10];
         }
-        public Map(string _mapName, int[,] _map)
+        public Map(string _mapName, int[] _map)
         {
             this._mapName = _mapName;
-            this._map = (int[,])_map.Clone();
+            this._map = (int[])_map.Clone();
         }
     }
 }
